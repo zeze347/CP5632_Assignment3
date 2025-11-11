550863eff497b6ade65c39f63bd94b9e53fdd2b5
Jiaxing Tian <jiaxing.tian@my.jcu.edu.au>
2025-11-12 07:07:53 +0800

    Add the get_valid_city function

--- assignment3.py
+++ assignment3.py
@@ -4,16 +4,16 @@ GEOCODE_URL = "https://geocoding-api.open-meteo.com/v1/search"
 FORECAST_URL = "https://api.open-meteo.com/v1/forecast"
 WIKI_SUMMARY_URL = "https://en.wikipedia.org/api/rest_v1/page/summary/"
 HEADERS = {
-    "User-Agent": "CP5632_Assignment_WeatherApp/1.0 (student project)"
+    "User-Agent": "CP5632_Assignment (student project)"
 }
 
 
 def main():
     while True:
-        city_name = input("Enter a city name: ").strip()
+        city_name = get_valid_city()
         location = geocode_city(city_name)
         if location is None:
-            print("City not found. Please try again.\n")
+            print("City not found. Please try again.")
             continue
         latitude, longitude = location
         break
@@ -27,6 +27,16 @@ def main():
     print(f"Here is a summary of {city_name}:")
     print(summary)
 
+def get_valid_city():
+    while True:
+        city_name = input("Enter a city name: ").strip()
+        if not city_name:
+            print("City name cannot be empty. Please try again.\n")
+            continue
+        elif city_name.isdigit():
+            print("City name cannot be numbers only. Please try again.\n")
+            continue
+        return city_name
 
 def geocode_city(city_name):
     try:
@@ -34,6 +44,10 @@ def geocode_city(city_name):
         location_result = requests.get(GEOCODE_URL, params=geocode_params)
         geocode_data = location_result.json()
         location_results = geocode_data.get("results", [])
+        result = location_results[0]
+        if result.get("feature_code") not in ("PPLC", "PPLA"):
+            print("Found a location but it's not classified as a city.")
+            return None
         latitude = location_results[0].get("latitude")
         longitude = location_results[0].get("longitude")
         return latitude, longitude
@@ -55,12 +69,19 @@ def get_weather(latitude, longitude):
 
 
 def get_city_summary(city_name):
-    page_title = city_name.replace(" ", "_")
-    url = WIKI_SUMMARY_URL + page_title
-    summary_result = requests.get(url, headers=HEADERS)
-    summary_data = summary_result.json()
-    summary = summary_data.get("extract")
-    return summary
+    try:
+        page_title = city_name.replace(" ", "_")
+        url = WIKI_SUMMARY_URL + page_title
+        summary_result = requests.get(url, headers=HEADERS)
+        summary_data = summary_result.json()
+        summary = summary_data.get("extract")
+        if summary is None:
+            print("Summary not found. Please try again.\n")
+        return summary
+
+    except KeyError:
+        return "Unexpected data format from Wikipedia."
+
 
 
 if __name__ == "__main__":
